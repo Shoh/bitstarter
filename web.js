f775259e839b438b47a8bab500ee77b9910766b7
Troy Haran <troyharan@yahoo.com>
2013-07-12 12:01:52 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'), "utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-    fs.readFileSync('index.html');
+    //response.send('Hello World 2!');
+    //response.send(fs.readFileSync('index.html'));
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
